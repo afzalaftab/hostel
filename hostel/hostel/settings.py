2ab21530b09e589f03f9572cd819ae02ac672a1a
Ram <panpaliaram@gmail.com>
2023-04-11 22:29:18 +0530

    hostel allotment ui done

--- hostel/hostel/settings.py
+++ hostel/hostel/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'mydb',
         'USER': 'root',
-        'PASSWORD': 'password',
+        'PASSWORD': 'Ram#123456',
         'HOST': 'localhost',
         'PORT': '3306',
     }
